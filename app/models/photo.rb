675d384c978b0a1d4fa389e2bd54e7e24a744071
Adam Thompson <adothompson@gmail.com>
2008-10-11 15:29:49 -0700

    photo resize fixes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -19,6 +19,9 @@
 #  height       :integer(11)     
 #
 
+require 'action_controller'
+require 'action_controller/test_process.rb'
+
 class Photo < ActiveRecord::Base
   belongs_to :person, :counter_cache => true
 
@@ -53,9 +56,24 @@ class Photo < ActiveRecord::Base
       w2, h2 = size.split('x').map(&:to_f)
       img.resize!(w2,h2)
     else
-      img.change_geometry(size.to_s) { |cols, rows, image| image.resize!(cols, rows) }
+      img.change_geometry(size.to_s) { image.resize!(cols<1 ? 1 : cols, rows<1 ? 1 : rows) }
     end
-    self.temp_path = write_to_temp_file(img.to_blob)
+    img.strip! unless attachment_options[:keep_profile]
+    temp_paths.unshift write_to_temp_file(img.to_blob)
+    #self.temp_path = write_to_temp_file(img.to_blob)
   end
 
+#         def resize_image(img, size)
+#           size = size.first if size.is_a?(Array) && size.length == 1 && !size.first.is_a?(Fixnum)
+#           if size.is_a?(Fixnum) || (size.is_a?(Array) && size.first.is_a?(Fixnum))
+#             size = [size, size] if size.is_a?(Fixnum)
+#             img.thumbnail!(*size)
+#           else
+#             img.change_geometry(size.to_s) { |cols, rows, image| image.resize!(cols<1 ? 1 : cols, rows<1 ? 1 : rows) }
+#           end
+#           img.strip! unless attachment_options[:keep_profile]
+#           temp_paths.unshift write_to_temp_file(img.to_blob)
+#         end
+  
+  
 end
